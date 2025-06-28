de32fde24153be090572a914c4d41a8db8fda6fb
farukulWD <farukgb1999@gmail.com>
2025-06-29 02:26:34 +0600

    write ai background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
+
 const Home = () => {
+  const [value, setValue] = useState("");
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job has been start");
+      },
+    })
+  );
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-3xl font-bold text-center">Hello world</h1>
+    <div className=" p-4 flex flex-col gap-y-2">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button className="" onClick={() => invoke.mutate({ value })}>
+        Inngest Background Jobs
+      </Button>
     </div>
   );
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    await step.sleep("wait-a-moment", "4s");
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer.  You summary in two words",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await summarizer.run(
+      `summarize the following text:${event.data.value}`
+    );
+    console.log(output);
+
+    return { output };
+  }
 );
